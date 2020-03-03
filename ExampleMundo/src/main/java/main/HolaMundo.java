998b860af6f1505cb200376b6c1eace2635f9b1d
josemi <josemi@josemi-OMEN-by-HP-Laptop>
2020-03-03 11:21:39 +0100

    Update Project

--- ExampleMundo/src/main/java/main/HolaMundo.java
+++ ExampleMundo/src/main/java/main/HolaMundo.java
@@ -1,6 +1,6 @@
 package main;
 
-public class main {
+public class HolaMundo {
 	public static void main(String args[]) {
 		System.out.println("HOla que pasa. Mi primer programa Java de nuevo, estamor ready");
 	}
