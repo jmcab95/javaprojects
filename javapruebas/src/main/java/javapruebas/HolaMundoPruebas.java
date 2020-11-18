a3302119a842ec1ac73e48eaf64c85a567635ce6
Jose Miguel Cabanillas Mansilla <jmcab95@users.noreply.github.com>
2020-11-18 18:04:37 +0100

    new class creation

---
+++ javapruebas/src/main/java/javapruebas/HolaMundoPruebas.java
@@ -0,0 +1,10 @@
+package javapruebas;
+
+public class HolaMundoPruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HOla mundo que tal todo");
+	}
+
+}
